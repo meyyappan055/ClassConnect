2b5ba0d12af59d15bbac44e063a3556ff90fbefd
meyyappan055 <meyyappan055@gmail.com>
2025-01-27 00:15:13 +0530

    fix : cors

--- app/backend/main.py
+++ app/backend/main.py
@@ -6,7 +6,9 @@ app = FastAPI()
 
 origins = [
     "http://127.0.0.1:5173",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://classconnect-liart.vercel.app/",
+    "https://classconnect-meyyappans-projects.vercel.app/"
 ]
 
 app.add_middleware(
