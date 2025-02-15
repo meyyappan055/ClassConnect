1dc4c94d06a68258d498ad34b136fb3328fc79e2
meyyappan055 <meyyappan055@gmail.com>
2025-02-15 21:51:28 +0530

    feat : add responsiveness

--- app/frontend/src/components/Card.jsx
+++ app/frontend/src/components/Card.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Card = ({ icon, title, text }) => {
   return (
-    <div className="flex flex-col bg-black text-white p-6 rounded-xl shadow-md border-1 max-w-lg h-44 m-4">
-
-      <div className="text-white mb-4 text-3xl">{icon}</div>
-      <h3 className="font-semibold text-lg mb-2">{title}</h3>
+    <div className="flex flex-col bg-black text-white p-6 rounded-xl shadow-md border border-gray-800 max-w-lg min-h-[176px] m-4">
+      <div className="flex-shrink-0 text-white mb-2 text-3xl">{icon}</div>
+      <h3 className="flex-shrink-0 font-semibold text-lg mb-2">{title}</h3>
       <p className="text-gray-400 text-sm">{text}</p>
-      
     </div>
   );
 };
--- app/frontend/src/pages/MainPage.jsx
+++ app/frontend/src/pages/MainPage.jsx
@@ -11,22 +11,24 @@ import Card from '@/components/Card'
 const MainPage = () => {
   return (
     <section >
-        <div className='mt-28 flex font-inter font-bold text-5xl justify-center items-center text-center'>
+        <div className='mt-28 flex font-inter font-bold text-3xl md:text-4-5xl justify-center items-center text-center'>
           Welcome to ClassConnect
         </div>
 
-        <div className='mx-96 mt-6 flex font-inter font-semibold text-base justify-center items-center text-center text-gray-300'>
+        <div className='md:max-w-3xl px-10 mx-auto mt-6 flex font-inter font-semibold text-base justify-center items-center text-center text-gray-300'>
         Student life is busy—between lectures, assignments, and extracurriculars, managing your time can be tough. Class Connect makes scheduling effortless, keeping you on top of your classes.
         </div>
 
-        <div className='flex justify-center mt-10 '>
-          <Button variant="outline" className="font-semibold rounded-xl text-base p-5">
-            <Link to="/login">Try it out</Link>
-            <CircleChevronRight className="mr-1 h-5 w-5" />
-          </Button>
-        </div>
-
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:justify-items-center mt-24 px-4'>
+        <Link to="/login">
+          <div className='flex justify-center mt-10 '>
+            <Button variant="outline" className="font-semibold rounded-xl text-base p-5">
+              Try it out
+              <CircleChevronRight className="mr-1 h-5 w-5" />
+            </Button>
+          </div>
+        </Link>
+  
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:mx-8 mt-16 md:mt-24 px-4 overflow-hidden'>
         <div className=''>
           <Card 
           icon = {<CalendarCheck />}
