70e96e45b9a8a57ac7d9b24255476ce23b3b8015
meyyappan055 <meyyappan055@gmail.com>
2025-03-08 13:10:36 +0530

    perf : improve browser launch

--- app/backend/services/login_script.py
+++ app/backend/services/login_script.py
@@ -59,7 +59,13 @@ def perform_login(page, email, password):
         return False
 
 def login_and_scrape(playwright: Playwright, email: str, password: str):
-    browser = playwright.chromium.launch(headless=True, args=["--disable-blink-features=AutomationControlled", "--disable-gpu"])
+    browser = playwright.chromium.launch(headless=False, args=[
+            "--disable-blink-features=AutomationControlled",
+            "--disable-gpu",
+            "--disable-dev-shm-usage",  
+            "--no-sandbox",  
+            "--disable-setuid-sandbox"
+        ])
     context = browser.new_context()
     context.route("**/*.{png,jpg,jpeg,svg,gif,woff,woff2,ttf}", lambda route: route.abort())
     page = context.new_page()
