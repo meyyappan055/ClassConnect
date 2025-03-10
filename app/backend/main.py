1c4c3d9757bb9549b24156cfb1669a95f510365d
meyyappan055 <meyyappan055@gmail.com>
2025-03-10 19:13:58 +0530

    feat : rate limiting handler

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI , HTTPException
-from api.v1.routes import login , generate_ical
+from fastapi import FastAPI, HTTPException
+from api.v1.routes import login, generate_ical
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
@@ -7,13 +7,18 @@ from slowapi.middleware import SlowAPIMiddleware
 from slowapi.errors import RateLimitExceeded
 from fastapi.responses import JSONResponse
 
-
 limiter = Limiter(key_func=get_remote_address)
-
 app = FastAPI()
 app.state.limiter = limiter
 app.add_middleware(SlowAPIMiddleware)
 
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request, exc):
+    return JSONResponse(
+        status_code=429,
+        content={"detail": "Too many requests, please try again later."}
+    )
 
 origins = [
     "http://127.0.0.1:5173",
